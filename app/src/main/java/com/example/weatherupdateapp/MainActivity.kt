60b3f0bae1bee09e4a3f11175ab31334755dcd19
ST10444782@vcconnect.edu.za <ST10444782@vcconnect.edu.za>
2024-06-10 15:40:54 +0200

    MainActivity,SplashActivity,and detailedVSActivit

--- app/src/main/java/com/example/weatherupdateapp/MainActivity.kt
+++ app/src/main/java/com/example/weatherupdateapp/MainActivity.kt
@@ -9,7 +9,7 @@ import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
 import android.widget.Toast
-import androidx.activity.enableEdgeTo
+import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 
 
